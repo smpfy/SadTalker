08ce05b2132c2219a4b9998f75150bfbb039c4cc
smpfy <arsmpfy@gmail.com>
2024-04-28 18:32:37 +0300

    TTS

--- app_sadtalker.py
+++ app_sadtalker.py
@@ -44,13 +44,13 @@ def sadtalker_demo(checkpoint_path='checkpoints', config_path='src/config', warp
                         with gr.Column(variant='panel'):
                             driven_audio = gr.Audio(label="Input audio", sources=["upload"], type="filepath")
 
-                        # if sys.platform != 'win32' and not in_webui:
-                        #     from src.utils.text2speech import TTSTalker
-                        #     tts_talker = TTSTalker()
-                        #     with gr.Column(variant='panel'):
-                        #         input_text = gr.Textbox(label="Generating audio from text", lines=5, placeholder="please enter some text here, we genreate the audio from text using @Coqui.ai TTS.")
-                        #         tts = gr.Button('Generate audio',elem_id="sadtalker_audio_generate", variant='primary')
-                        #         tts.click(fn=tts_talker.test, inputs=[input_text], outputs=[driven_audio])
+                        if sys.platform != 'win32' and not in_webui:
+                            from src.utils.text2speech import TTSTalker
+                            tts_talker = TTSTalker()
+                            with gr.Column(variant='panel'):
+                                input_text = gr.Textbox(label="Generating audio from text", lines=5, placeholder="please enter some text here, we genreate the audio from text using @Coqui.ai TTS.")
+                                tts = gr.Button('Generate audio',elem_id="sadtalker_audio_generate", variant='primary')
+                                tts.click(fn=tts_talker.test, inputs=[input_text], outputs=[driven_audio])
                             
             with gr.Column(variant='panel'): 
                 with gr.Tabs(elem_id="sadtalker_checkbox"):
